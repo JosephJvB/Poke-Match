c03f197d4e812f5d3c2db44701ccdb94e4d6e284
JosephJvB <jo-van-bo@hotmail.com>
2017-12-16 09:34:37 +1300

    read write mons

---
+++ server/pokeScrape/scrape.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer')
+
+async function scrape (mon) {
+  const browser = await puppeteer.launch({headless: false})
+  const page = await browser.newPage()
+  await page.goto(`https://bulbapedia.bulbagarden.net/wiki/${mon}_(Pok%C3%A9mon)`)
+
+  const pic = await page.evaluate(() => {
+    let sauce = document.body.querySelector('a.image').innerHTML.split('src="//')[1].split('" ')[0]
+    return sauce
+  })
+
+  browser.close()
+  console.log(mon, ': ', pic)
+  return pic
+}
+
+module.exports = { scrape }
+
+// mostly works but annoying ad means I have to click it, OR maybe I can automate that too.
