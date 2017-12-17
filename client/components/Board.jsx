da0e6315f5d6f8f77c5b7e73ab54be537ac36e94
JosephJvB <jo-van-bo@hotmail.com>
2017-12-18 08:09:33 +1300

    8 random imgs

--- client/components/Board.jsx
+++ client/components/Board.jsx
@@ -3,16 +3,34 @@ import { connect } from 'react-redux'
 
 import Cell from './Cell'
 import { cells } from '../../server/cellData'
+import pokePics from '../../server/pokeScrape/pokeLibrary.json'
 
 class Board extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-
+      cells: []
     }
     // binds go here:
+    this.scrumble = this.scrumble.bind(this)
   }
   // functions go here:
+  componentDidMount () {
+    this.scrumble([])
+  }
+
+  scrumble (arr) {
+    let scrambleCells = arr.length > 0 ? arr : []
+    const newPokemon = pokePics.filter(img => !this.state.cells.find(i => i === img))
+    let idx = Math.floor(Math.random() * newPokemon.length)
+    scrambleCells.push(newPokemon[idx])
+    if (scrambleCells.length === 8) {
+      console.log(scrambleCells)
+      return this.setState({ cells: scrambleCells })
+    } else {
+      this.scrumble(scrambleCells)
+    }
+  }
 
   render () {
     const boardState = this.props.temp.length < 2 ? 'open' : 'closed'
